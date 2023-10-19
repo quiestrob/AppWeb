0cb80775dcff98d34e6b6c4b262898a659256c6b
RobertoBerrio <robertocarlosberrio@gmail.com>
2023-10-19 02:15:16 -0500

    Se agrego la clase fundador.

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -7,6 +7,7 @@
     include_once $_SERVER['DOCUMENT_ROOT'].'/proaulav2/models/Profesor.php';
     include_once $_SERVER['DOCUMENT_ROOT'].'/proaulav2/models/Estado.php';
     include_once $_SERVER['DOCUMENT_ROOT'].'/proaulav2/models/Inscripcion.php';
+    include_once $_SERVER['DOCUMENT_ROOT'].'/proaulav2/models/Fundador.php';
 
     class LoginController {
         public static function executeAction() {
@@ -89,9 +90,33 @@
                                 }  
                             } catch (Exception $error) {
                                 if (strstr($error->getMessage(), $identification)) {
-                                    $msj = "El usuario no existe.";
+                                    try {
+                                        $f = Fundador::find($identification);
+                
+                                        if ($f->contraseña == $pass) {
+                                            $f = serialize($f);
+                                            $_SESSION['usuario.login'] = $f;
+                                            $_SESSION['usuario.type'] = 'Administrador';
+                        
+                                            header("Location: ../root/pages/validation_inscription.php");
+                                            exit;
+                                        } else {
+                                            $_SESSION['usuario.login'] = null;
+                                            header("Location: ../root/pages/login.php?msj=Contraseña incorrecta.");
+                                            exit;
+                                        }  
+                                    } catch (Exception $error) {
+                                        if (strstr($error->getMessage(), $identification)) {
+                                            $msj = "El usuario no existe.";
+                                        } else {
+                                            $msj = "Ocurrio un error.";
+                                        }
+        
+                                        header("Location: ../root/pages/login.php?msj=$msj");
+                                        exit;
+                                    }
                                 } else {
-                                    $msj = "Ocurrio un error.";
+                                    $msj = $error->getMessage();
                                 }
 
                                 header("Location: ../root/pages/login.php?msj=$msj");
--- models/Inscripcion.php
+++ models/Inscripcion.php
@@ -5,7 +5,7 @@
     class Inscripcion extends ActiveRecord\Model {
         public static $table_name = 'Inscripciones';
         public static $belongs_to = array (
-            array('Acudido', 'Estado')
+            array('Acudido', 'Estado', 'Fundador')
         );
     }
 
